f17bc271ee856ef20ac8fd207b38bed0c6f154d1
kbrother <00076289@kaist.ac.kr>
2023-12-24 13:59:58 +0000

    add mappings for 70

--- init_order.py
+++ init_order.py
@@ -79,10 +79,10 @@ def reorder(input_tensor):
     
     return change_order, final_orders
 
-# python init_order.py -lp ../data/uber -sp mapping/uber_60_model2tens -de 0 -k 60
-# python init_order.py -lp ../data/airquality -sp mapping/airquality_60_model2tens -de 0 -k 60
-# python init_order.py -lp ../data/action -sp mapping/action_60_model2tens -de 0 -k 60
-# python init_order.py -lp ../data/pems -sp mapping/pems_60_model2tens -de 0 -k 60
+# python init_order.py -lp ../data/uber -sp mapping/uber_70_model2tens -de 0 -k 70
+# python init_order.py -lp ../data/airquality -sp mapping/airquality_70_model2tens -de 0 -k 70
+# python init_order.py -lp ../data/action -sp mapping/action_70_model2tens -de 0 -k 70
+# python init_order.py -lp ../data/pems -sp mapping/pems_70_model2tens -de 0 -k 70
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-lp', '--load_path', type=str)
